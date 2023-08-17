7f6746555cdc54154f90373c5f2ceabef8af6903
Mohamed Nasir Mohamed Shaja <shaja.n@intervest.lk>
2023-08-17 22:26:47 +0530

    add Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
   const [cartIsShow, setCartIsShown] = useState(false);
@@ -12,9 +13,15 @@ const App = () => {
     setCartIsShown(false);
   };
 
-  return <CartProvider>
-    <Cart></Cart>
-  </CartProvider>;
+  return (
+    <CartProvider>
+      {cartIsShow && <Cart onClose={hideCartHandler}></Cart>}{" "}
+      {/* <Header onShowCart={showCartHandler} />{" "} */}
+      <main>
+        {/* <Meals /> */}
+      </main>
+    </CartProvider>
+  );
 };
 
 export default App;
